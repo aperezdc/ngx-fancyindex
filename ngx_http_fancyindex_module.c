566ad22a619d29de04bb6a70edbd2a7b3e106105
Tim Gates <tim.gates@iress.com>
2020-12-18 07:04:58 +1100

    docs: fix simple typo, configration -> configuration
    There is a small typo in ngx_http_fancyindex_module.c.
    
    Should read `configuration` rather than `configration`.

--- ngx_http_fancyindex_module.c
+++ ngx_http_fancyindex_module.c
@@ -496,8 +496,8 @@ static ngx_http_module_t  ngx_http_fancyindex_module_ctx = {
     NULL,                                  /* create server configuration */
     NULL,                                  /* merge server configuration */
 
-    ngx_http_fancyindex_create_loc_conf,   /* create location configration */
-    ngx_http_fancyindex_merge_loc_conf     /* merge location configration */
+    ngx_http_fancyindex_create_loc_conf,   /* create location configuration */
+    ngx_http_fancyindex_merge_loc_conf     /* merge location configuration */
 };
 
 
